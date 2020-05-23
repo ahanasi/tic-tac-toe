fd9680f6fcdcba38fee74d71b92ba2986fb00aa5
Ahana <ahanasi@seas.upenn.edu>
2020-05-23 15:12:44 -0700

    Create print board method

--- board.rb
+++ board.rb
@@ -1,7 +1,16 @@
 class Board
-  attr_accessor :grid
+  attr_accessor :board
 
-  def initialize(grid)
-    @grid = grid
+  def initialize()
+    @board = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9]
+    ]
   end
+
+  def printb()
+    @board.each_cons(1) {|a,b,c| puts "#{a}  #{b}  #{c}\n"}
+  end
+
 end
